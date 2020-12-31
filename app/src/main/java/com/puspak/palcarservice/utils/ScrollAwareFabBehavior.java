7aee70faf5e378119ff1e773ed4a053558359bb4
local <Localhost>
2020-12-31 12:39:13 +0530

    Initial Commit

---
+++ app/src/main/java/com/puspak/palcarservice/utils/ScrollAwareFabBehavior.java
@@ -0,0 +1,48 @@
+package com.puspak.palcarservice.utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class ScrollAwareFabBehavior extends FloatingActionButton.Behavior {
+
+    public ScrollAwareFabBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
+
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View directTargetChild, @NonNull View target, int axes, int type) {
+        return true;
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, FloatingActionButton child, View dependency) {
+        return dependency instanceof RecyclerView;
+
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull FloatingActionButton child, @NonNull View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int type) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type);
+
+        if (dyConsumed > 0 && child.getVisibility() == View.VISIBLE) {
+            child.hide(new FloatingActionButton.OnVisibilityChangedListener() {
+                           @Override
+                           public void onHidden(FloatingActionButton fab) {
+                               super.onHidden(fab);
+                               fab.setVisibility(View.INVISIBLE);
+                           }
+                       }
+            );
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
+            child.show();
+        }
+    }
+}
