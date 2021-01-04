618535504ca3292250571e51ccdbf9c79265a4cd
local <Localhost>
2021-01-04 14:38:56 +0530

    [Voyage] 1. Navigation View Added 2. Style Changed 3. New App font added 4. Voyage Point added

--- app/src/main/java/com/puspak/habraCarService/MainActivity.java
+++ app/src/main/java/com/puspak/habraCarService/MainActivity.java
@@ -3,11 +3,15 @@ package com.puspak.habraCarService;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -15,6 +19,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
@@ -24,6 +30,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private NavigationView navigationView;
     private BottomNavigationView bottomNavigationView;
     private FloatingActionButton floatingactionbutton;
+    private CircleImageView imgUser;
+    private TextView txtUserName;
+    private TextView txtUserMobile;
 
 
     @Override
@@ -35,6 +44,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawerlayout = findViewById(R.id.drawerLayout);
         navigationView = findViewById(R.id.navigationView);
         View navView = navigationView.getHeaderView(0);
+        imgUser = navView.findViewById(R.id.ivLogo);
+        txtUserName = navView.findViewById(R.id.userName);
+        txtUserMobile = navView.findViewById(R.id.userPhone);
         bottomNavigationView = findViewById(R.id.bottomNavigation);
         floatingactionbutton = findViewById(R.id.btFab);
 
@@ -45,13 +57,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private void setupToolbar() {
         setSupportActionBar(toolbar);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
     }
 
     private void setupNavigationDrawer() {
         drawerToggle = new ActionBarDrawerToggle(
                 this,drawerlayout,toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerlayout.setDrawerListener(drawerToggle);
+        drawerlayout.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
 
         navigationView.setNavigationItemSelectedListener(MainActivity.this);
@@ -60,5 +72,37 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        drawerlayout =findViewById(R.id.drawerLayout);
+        if(drawerlayout.isDrawerOpen(GravityCompat.START)) {
+            drawerlayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.nav_menu_wallet, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
